8775251e746a19f9478299c3e74b48fd860ce415
Dmitry <d2013.dima@mail.ru>
2025-03-08 02:12:46 +0500

    feat: add pagination component

---
+++ src/widgets/Pagination/ui/Pagination.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import { usePagination } from "../lib/usePagination";
+
+import { PaginationDropDown } from "./PaginationDropDown";
+import { PaginationNums } from "./PaginationNums";
+
+type PaginationProps = {
+  totalItems: number;
+};
+
+export function Pagination({ totalItems }: PaginationProps) {
+  const {
+    currentPage,
+    itemsPerPage,
+    onChangeCurrentPage,
+    onChangeItemsPerPage,
+  } = usePagination();
+
+  const totalPages = Math.ceil(totalItems / Number(itemsPerPage));
+
+  return (
+    <div className="flex items-center gap-2 justify-between rounded-md p-2 bg-background text-popover-foreground">
+      <PaginationNums
+        currentPage={Number(currentPage)}
+        onPageChange={onChangeCurrentPage}
+        totalPages={totalPages}
+      />
+      <PaginationDropDown
+        itemsPerPage={Number(itemsPerPage)}
+        onItemsPerPageChange={onChangeItemsPerPage}
+      />
+    </div>
+  );
+}
---
+++ src/widgets/Pagination/ui/PaginationNums.tsx
@@ -0,0 +1,71 @@
+import { ChevronLeft, ChevronRight, MoreHorizontal } from "lucide-react";
+
+import { cn } from "@/shared/lib/css";
+
+import { getPageNumbers } from "../lib/getPageNumbers";
+
+type PaginationNumsProps = {
+  totalPages: number;
+  currentPage: number;
+  onPageChange: (page: number) => void;
+};
+
+export const PaginationNums = ({
+  currentPage,
+  onPageChange,
+  totalPages,
+}: PaginationNumsProps) => {
+  //Массив, который отображаем в зависимости от кол-ва страниц
+  const pageNumbers = getPageNumbers(totalPages, currentPage);
+
+  return (
+    <div className="flex items-center space-x-2">
+      <button
+        onClick={() => onPageChange(Math.max(1, currentPage - 1))}
+        disabled={currentPage === 1}
+        className={cn(
+          "p-1 cursor-pointer rounded-md  disabled:opacity-50 transition-colors duration-300 ease-out",
+          currentPage === 1 ? "cursor-no-drop" : "hover:bg-foreground",
+        )}
+      >
+        <ChevronLeft className="h-4 w-4" />
+      </button>
+
+      {pageNumbers.map((page, index) => {
+        if (page === "ellipsis-start" || page === "ellipsis-end") {
+          return (
+            <span key={`ellipsis-${index}`} className="px-2">
+              <MoreHorizontal className="h-4 w-4" />
+            </span>
+          );
+        }
+
+        return (
+          <button
+            key={index}
+            onClick={() => onPageChange(Number(page))}
+            className={cn(
+              "min-w-[28px] h-7 px-2 rounded-md cursor-pointer font-bold transition-colors duration-300 ease-out",
+              currentPage === page
+                ? "bg-primary-foreground text-background"
+                : "hover:bg-foreground",
+            )}
+          >
+            {page}
+          </button>
+        );
+      })}
+
+      <button
+        onClick={() => onPageChange(Math.min(totalPages, currentPage + 1))}
+        disabled={currentPage === totalPages}
+        className={cn(
+          "p-1 cursor-pointer rounded-md  disabled:opacity-50 transition-colors duration-300 ease-out",
+          currentPage === totalPages ? "cursor-no-drop" : "hover:bg-foreground",
+        )}
+      >
+        <ChevronRight className="h-4 w-4" />
+      </button>
+    </div>
+  );
+};
