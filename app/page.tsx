beccf1fc1092b0b46ddec812048657ae1e8d9781
sashavna <sashavna99@gmail.com>
2025-03-09 15:33:40 +0100

    refactor: improve Button component logic and styling

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,15 @@
 "use client";
 
+import { Button } from "@/shared/ui";
 import { Pagination } from "@/widgets/Pagination";
 import { RecaptchaForm } from "@/widgets/Recaptcha-Form";
 
 export default function Home() {
   return (
-    <div className={`flex justify-center mt-56`}>
+    <div className={`flex justify-center mt-56 flex-col items-center`}>
       <Pagination totalItems={101} />
       <RecaptchaForm />
+      <Button type="link">Button</Button>
     </div>
   );
 }
