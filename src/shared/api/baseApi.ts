28cf07cfc4181b27a893d31c7d2f102f3a8bd17d
Oleg010-web <nesteroff_oleg@mail.ru>
2025-03-10 08:28:51 +0300

    MI-3 feat: add base api

---
+++ src/shared/api/baseApi.ts
@@ -0,0 +1,41 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+import { setAppError } from "../model/appSlice";
+
+export const baseApi = createApi({
+  reducerPath: "ingramApi",
+  baseQuery: async (args, api, extraOptions) => {
+    const result = await fetchBaseQuery({
+      baseUrl: "https://payments.myin-gram.ru/api/v1",
+      prepareHeaders: () => {},
+    })(args, api, extraOptions);
+    let error = "Произошла ошибка";
+
+    if (result.error) {
+      switch (result.error.status) {
+        case "FETCH_ERROR":
+        case "PARSING_ERROR":
+        case "CUSTOM_ERROR":
+          error = result.error.error;
+          break;
+
+        case 403:
+          error = "403 Ошибка доступа. Проверь API-KEY";
+          break;
+
+        case 400:
+          error = (result.error.data as { message: string }).message;
+          break;
+
+        default:
+          error = JSON.stringify(result.error);
+          break;
+      }
+      api.dispatch(setAppError({ error }));
+    }
+    return result;
+  },
+  endpoints: () => ({}),
+  tagTypes: [],
+  refetchOnFocus: true,
+});
