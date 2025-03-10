fb88affbcc44a514393617b77dfdab16b934ef72
Irene Volodko <ice7aye@gmail.com>
2025-03-10 06:12:24 +0300

    feat: select-box

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,23 +1,20 @@
 "use client";
 
-import {
-  DatePicker,
-  DateRangePicker,
-  Input,
-  Pagination,
-  RecaptchaForm,
-} from "@/shared/ui";
+import {DatePicker, DateRangePicker, Input, Pagination, RecaptchaForm,} from "@/shared/ui";
+import {SelectBox} from "@/shared/ui/SelectBox/SelectBox";
 
 export default function Home() {
   return (
     <div
       className={`flex gap-72 flex-col pt-[80px] px-9 mx-auto w-full max-w-[1180px]`}
     >
-      <Pagination totalItems={101} />
-      <RecaptchaForm />
-      <Input type="search" />
-      <DateRangePicker />
-      <DatePicker />
+      <Pagination totalItems={101}/>
+      <RecaptchaForm/>
+      <SelectBox placeholder={"Select-box"} label={"Select-box"}
+                 options={[{id: "Select-box1", value: "Select-box1", title: "Select-box1"}]}/>
+      <Input type="search"/>
+      <DateRangePicker/>
+      <DatePicker/>
     </div>
   );
 }
