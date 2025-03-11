c595011dc3d606742d258db91d0d4b75adca81b9
Irene Volodko <ice7aye@gmail.com>
2025-03-11 22:34:03 +0300

    fix: selectbox

--- src/shared/ui/index.ts
+++ src/shared/ui/index.ts
@@ -9,3 +9,5 @@ export { Pagination } from "./Pagination/ui/Pagination";
 export { Button, buttonVariants } from "./Button/Button";
 export { SwitchButton } from "./SwitchButton/SwitchButton";
 export { Input } from "./Input/Input";
+
+export { SelectBox } from "./SelectBox/SelectBox";
