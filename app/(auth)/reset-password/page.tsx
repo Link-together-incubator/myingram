ab0119fbde6a23295b70783ecb7ec3bd7ec44e57
Dmitry <d2013.dima@mail.ru>
2025-03-15 04:10:24 +0500

    fix: suspense

--- app/(auth)/reset-password/page.tsx
+++ app/(auth)/reset-password/page.tsx
@@ -1,14 +1,13 @@
 'use client'
 
+import { Suspense } from 'react'
+
 import { ResetPasswordForm } from '@/features/Auth/PasswordRecovery'
 
 export default function PasswordRecovery() {
-  // const router = useRouter()
-  // const params = useSearchParams()
-  // const token = params.get('code')
-  // if (!token) {
-  //   router.push('/')
-  // }
-
-  return <ResetPasswordForm />
+  return (
+    <Suspense>
+      <ResetPasswordForm />
+    </Suspense>
+  )
 }
