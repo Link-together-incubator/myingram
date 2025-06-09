67295b5e006974105350086da7051ce76d50a4bc
sashavna <sashavna99@gmail.com>
2025-06-09 21:48:41 +0200

    fix: change type

--- src/entities/profile/api/profileApi.ts
+++ src/entities/profile/api/profileApi.ts
@@ -1,8 +1,8 @@
+import {
+  UserProfile,
+  EditUserProfile,
+} from '@/entities/profile/model/profile.types'
 import { baseApi } from '@/shared/api/baseApi'
-import {UserProfile,  EditUserProfile } from "@/entities/profile/model/profile.types";
-import {GeneralInformationData} from "@/entities/profile/model/GeneralInformationSchem";
-
-import { EditUserProfile, UserProfile } from '../profile.types'
 
 export const profileApi = baseApi.injectEndpoints({
   endpoints: (builder) => ({
@@ -10,7 +10,6 @@ export const profileApi = baseApi.injectEndpoints({
       query: (userId) => `profile/${userId}`,
       providesTags: (result) => [{ type: 'UserProfile', id: result?.id }],
     }),
-    editUserProfile: builder.mutation<EditUserProfile, FormData>({
     editUserProfile: builder.mutation<EditUserProfile, FormData>({
       query: (formData) => ({
         url: 'profile/edit',
