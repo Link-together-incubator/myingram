0150590765d48ec695ed73fdac28f6cb7a0b04ca
sashavna <sashavna99@gmail.com>
2025-08-29 17:42:10 +0200

    style: apply eslint fixes

--- src/entities/post/ui/PostModal/ui/PostModalServerSide.tsx
+++ src/entities/post/ui/PostModal/ui/PostModalServerSide.tsx
@@ -1,5 +1,5 @@
 'use client'
-import { useEffect, useLayoutEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 
 import { AppAlertClient } from '@/entities/post/ui/AppAlertClient/AppAlertClient'
 import { ACCESS_TOKEN } from '@/shared/constants/const'
